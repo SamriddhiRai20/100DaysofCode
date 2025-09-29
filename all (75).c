1352a78780a729dfefc647805a08f6850c77dc7d
SamriddhiRai20 <samriddhirai1720@gmail.com>
2025-09-29 23:31:16 +0530

    Add files via upload

---
+++ all (75).c
@@ -0,0 +1,30 @@
+/*
+WAP to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    int i,j,k;
+    int rows=5;
+    for(i=1;i<=rows;++i)
+    {
+        for(j=1;j <= 2 * (i-1);++j)
+        {
+            printf(" ");
+        } 
+          for(k=1;k <= rows - i + 1;++k)
+          {
+              printf("*");
+          }
+    printf("\n");
+    }
+return 0;
+}
+
