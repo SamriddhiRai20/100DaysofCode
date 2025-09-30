0e782de6e929fcff2ffed1b0c64e579a798486ae
SamriddhiRai20 <samriddhirai1720@gmail.com>
2025-10-01 01:07:22 +0530

    Add files via upload

---
+++ all (86).c
@@ -0,0 +1,42 @@
+//WAP to search for an element un an array using linear search.
+
+#include <stdio.h>
+
+int main() 
+{
+ int a[100];
+ int search,i,n;
+ 
+ 
+ printf("Input the number of elements to be stored in the array:\n");
+ scanf("%d", &n);
+ 
+ printf("Input %d elements in the array:\n", n);
+ for(i=0;i<n;i++)
+ {
+     printf("element %d: \n",i);
+     scanf("%d", &a[i]);
+ }
+ 
+    printf("Enter the number to search:\n");
+    scanf("%d", &search);
+
+ for(i=0;i<n;i++)
+ {
+    if(a[i] == search)
+    {
+      printf("%d is found at index %d.\n", search, (i));
+      break;
+    }
+ }
+ 
+ if(i == n)
+ {
+   printf("%d is not found in the array.\n", search);  
+ }
+ 
+ 
+return 0;
+
+}
+
