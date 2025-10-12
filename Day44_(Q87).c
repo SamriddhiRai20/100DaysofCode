b8ac01945d85b46df03ba0a31db7eddc6dd399f1
SamriddhiRai20 <samriddhirai1720@gmail.com>
2025-10-12 11:58:17 -0700

    Rename all (34).c to Day44_(Q87).c

--- Day44_(Q87).c
+++ Day44_(Q87).c
@@ -1,21 +1,31 @@
+//Count spaces,digits, and special charcters in a string.
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-int main() {
+int main()
+{
     char str[200];
     int spaces = 0, digits = 0, special = 0;
 
     printf("Enter a string:\n ");
     fgets(str, sizeof(str), stdin); 
 
-    for (int i = 0; i < strlen(str); i++) {
+    for (int i = 0; i < strlen(str); i++) 
+    {
         if (str[i] == ' ')
+        {
             spaces++;
+        }
         else if (isdigit(str[i]))
+        {
             digits++;
+        }
         else if (!isalpha(str[i]) && str[i] != '\n')
+        {
             special++;
+        }
     }
 
     printf("Spaces: %d\n", spaces);
