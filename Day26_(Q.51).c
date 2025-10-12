a365f50b361857beb1b7aa2c279ae79093825b5a
SamriddhiRai20 <samriddhirai1720@gmail.com>
2025-10-12 11:40:09 -0700

    Update and rename all (25).c to Day26_(Q.51).c

--- Day26_(Q.51).c
+++ Day26_(Q.51).c
@@ -1,18 +1,31 @@
+/*
+    WAP to print the following pattern:
+        5
+       45
+      345
+     2345
+    12345
+*/
+
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, j, k;
 
     // Loop for each row
-    for (i = 5; i >= 1; i--) {
+    for (i = 5; i >= 1; i--)
+        {
 
         // Print leading spaces
-        for (j = 1; j < i; j++) {
+        for (j = 1; j < i; j++) 
+        {
             printf("  "); // two spaces for alignment
         }
 
         // Print numbers from i to 5
-        for (k = i; k <= 5; k++) {
+        for (k = i; k <= 5; k++)
+        {
             printf("%d ", k);
         }
 
