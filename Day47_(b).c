b91d2386de7c13a194e13a536af0d41a2aa11e01
SamriddhiRai20 <samriddhirai1720@gmail.com>
2025-10-12 11:11:44 -0700

    Update and rename all (17).c to Day47_(b).c

--- Day47_(b).c
+++ Day47_(b).c
@@ -1,19 +1,24 @@
+//Find the longest word in a sentence.
+
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     char sentence[200], word[50], longest[50];
     int i = 0, j = 0, maxLength = 0;
 
     printf("Enter a sentence: ");
-    gets(sentence);  // you can use fgets(sentence, sizeof(sentence), stdin) for safety
+    gets(sentence);  
 
     for (i = 0; ; i++) {
-        if (sentence[i] != ' ' && sentence[i] != '\0') {
+        if (sentence[i] != ' ' && sentence[i] != '\0')
+        {
             word[j++] = sentence[i];
         } else {
             word[j] = '\0';  // end of a word
-            if (strlen(word) > maxLength) {
+            if (strlen(word) > maxLength)
+            {
                 maxLength = strlen(word);
                 strcpy(longest, word);
             }
@@ -21,12 +26,13 @@ int main() {
             if (sentence[i] == '\0')
                 break;  // end of sentence
         }
-    } // ✅ <-- this closing brace was probably missing
+    } 
 
     printf("The longest word is: %s\n", longest);
     printf("Length of the longest word: %d\n", maxLength);
 
     return 0;
 } 
+
 
 
